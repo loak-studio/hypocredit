dc0b1a751fe8a50d6bd8370af69a0c22d537480b
Benjamin Geets <benjamingeets@tutamail.com>
2022-11-04 14:55:39 +0100

    refactor: prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 export default defineConfig({
-  site: 'https://hypo-credit.be',
-  integrations: [sitemap(), compress()]
+  site: "https://hypo-credit.be",
+  integrations: [sitemap(), compress()],
 });
